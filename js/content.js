39c260a6b4a5ca8c7610575036388954ab8f709f
r05 <mynameis.rost@gmail.com>
2016-03-11 08:08:55 +0200

    implement multilanguage and language switcher

---
+++ js/content.js
@@ -0,0 +1,236 @@
+window.data = {
+  light: {
+    "EN":"Light panel for sketches drawing",
+    "DE":"Lichtbox für Skizzen",
+    "ES":"Panel de luz para bocetos",
+    "Dutch":"Lichtpaneel voor het maken van schetsen"
+  },
+  lifelong: {
+    "EN":"Lifelong warranty",
+    "DE":"Lebenslange Garantie",
+    "ES":"Garantía de por vida",
+    "Dutch":"levenslange garantie"
+  },
+  free: {
+    "EN":"Free shipping",
+    "DE":"Versandkostenfrei",
+    "ES":"Envio gratis",
+    "Dutch":"gratis verzending"
+  },
+  buy:{
+    "EN":"Buy",
+    "DE":"Kaufen",
+    "ES":"Comprar",
+    "Dutch":"Kopen"
+  },
+  what:{
+    "EN":"What.",
+    "DE":"Was.",
+    "ES":"Qué.",
+    "Dutch":"Wat."
+  },
+  why:{
+    "EN":"Why.",
+    "DE":"Warum.",
+    "ES":"Por qué.",
+    "Dutch":"Waarom."
+  },
+  who:{
+    "EN":"Who.",
+    "DE":"Wer",
+    "ES":"Quién.",
+    "Dutch":"Wie."
+  },
+  wassup1:{
+    "EN":"Wassup! We are Saw Workshop's Team!",
+    "DE":"Was geht aaab? Wir sind Saw Workshop's Team!",
+    "ES":"¡Qué pasa! ¡Somos el equipo de Saw Workshop!",
+    "Dutch":"Wassup! We zijn het Saw Workshop team!"
+  },
+  wassup2:{
+    "EN":"We produce top-quality handcrafted light-panels. It is a useful tool for tattooists. Each artist can draw sketches faster and more convenient with our stuff.",
+    "DE":"Wir stellen hochqualitative handgefertigte Licht Paneele her. Es ist ein nützliches Tool für Tätowierer. Jeder Zeichner kann damit schneller und bequemer skizzieren. ",
+    "ES":"Producimos paneles de luz hechos a mano de calidad alta, una herramienta muy útil para tatuadores. Permitiendo que los artistas puedan dibujar bocetos más rápidos y cómodamente. \n",
+    "Dutch":"Wij produceren top-kwaliteit, handgemaakte lichtpanelen. Dit zijn perfecte tools voor tattoo artists. Iedere artist kan met onze panelen sneller en gemakkelijker goede schetsen maken."
+  },
+  draw1:{
+    "EN":"Draw wherever you want.",
+    "DE":"Zeichne wo auch immer du willst",
+    "ES":"Dibuja lo que quieras.",
+    "Dutch":"Teken waar je maar wilt."
+  },
+  draw2:{
+    "EN":"No need to find where to draw anymore - your only place for drawing always with you.",
+    "DE":"Du brauchst keinen Platz mehr zu finden um zu Zeichnen - dein einziger Platz den du brauchst hast du immer bei dir.",
+    "ES":"No volverás a necesitar un sito específico para dibujar - tu sitio de trabajo irá contigo a dónde vayas. ",
+    "Dutch":"Nooit meer zoeken naar een tekenplek - je enige plek van tekenen heb je nu altijd bij je."
+  },
+  finding1:{
+    "EN":"Finding the perfect present for a tattoo artist is extremely hard.",
+    "DE":"Es passt für jeden Jedes Stück im Leben eines.",
+    "ES":"De fácil uso para todos.",
+    "Dutch":"Het is heel moeilijk om het perfecte kado voor een tattoo artist te vinden."
+  },
+  finding2:{
+    "EN":"All of they are unique personalities who choose their equipment\nmeticulously. But there is one gift that is universal for anyone who draws: a well-built LED light panel for making their sketches.",
+    "DE":"Tätowierers ist wirklich einzigartig und nicht zu verschenken, aber nicht so bei einem LED Panel. Das LED Panel wird der beste Freund für jeden einzelnen Tätowierer, weil jeder Tätowierer Skizzen zeichnet.",
+    "ES":"Las herramientas de todo artista del tattoo son únicas y no intercambiables, pero no son un panel de LED. Lo paneles de LED se convertirán en el mejor amigo de cualquier tatuador ya que es la mejor forma para dibujar bocetos. А",
+    "Dutch":"Elke tattoo artist is uniek en kiest zijn apparatuur vaak helemaal\nzelf. Maar er is één kado dat geschikt is voor elke tattoo artist of tekenaar: een goed-gebouwd LED lichtpaneel waarop ze hun schetsen\nkunnen maken"
+  },
+  handmade1:{
+    "EN":"Handmade.",
+    "DE":"Handgefertigt",
+    "ES":"Hechos a mano.",
+    "Dutch":"Handgemaakte kwaliteit."
+  },
+  handmade2:{
+    "EN":"All panels are handcrafted at a local workshop in Kyiv, combining true Ukrainian love and European quality.",
+    "DE":"Alle Panels werden in einer Werkstatt in Kiew handgefertigt. Dabei kombinieren wir wahre ukrainische Liebe mit europäischer Qualität.",
+    "ES":"Toda los paneles son hechos a mano en una empresa local de Kiev con una mezcla de amor desde Ucrania y calidad Europea. ",
+    "Dutch":"Alle lichtpanelen zijn handgemaakt in een locale workshop in Kiev. Wij combineren Oekraïnse liefde voor techniek met Europese kwaliteit."
+  },
+  frees1:{
+    "EN":"Frees up time.",
+    "DE":"Mehr Zeit fürs Wesentliche",
+    "ES":"Aumentará tu tiempo libre.",
+    "Dutch":"Meer tijd over."
+  },
+  frees2:{
+    "EN":"Tattoo artists draw sketches 30-60% faster through the use of panels.",
+    "DE":"Tätowierer zeichnen Skizzen 30-60% schneller mit Hilfe der Panels.",
+    "ES":"Los tatuadores dibujan boceto un 30 -  60% más rápido con estos paneles.",
+    "Dutch":"Tattoo artists tekenen hun schetsen 30-60% sneller door het gebruik van onze panelen."
+  },
+  designed1:{
+    "EN":"Designed for sketching.",
+    "DE":"Designed fürs Skizzieren",
+    "ES":"Diseñado para dibujar.",
+    "Dutch":"Ontworpen voor schetsen."
+  },
+  designed2:{
+    "EN":"Workspace of the panel has size equals to A3 paper. Also, it is divided into centimetre squares. This is especially good for drawing geometric patterns.\n",
+    "DE":"Die Arbeitsfläche des Panels hat A3 Format. Außerdem ist es unterteilt in zentimetergroße Quadrate. Dies ist besonders gut um geometrische Muster zu zeichnen.",
+    "ES":"El área de trabajo de los paneles es equivalente a un papel A3. También, dividido en centímetros cuadrados, lo que es especialmente ideal para patrones geométricos.",
+    "Dutch":"Het werkoppervlak van het paneel is even groot als een A3-vel. Om schetsen makkelijker te maken hebben wij een centimeterraster ingebouwd in ons lichtpaneel. Hierdoor wordt het heel gemakkelijk om geometrische patronen te tekenen."
+  },
+  brightness1:{
+    "EN":"Brightness control.",
+    "DE":"Helligkeit regelbar",
+    "ES":"Control de brillo.",
+    "Dutch":"Instelbare helderheid."
+  },
+  brightness2:{
+    "EN":"The panel is connected to the power supply of tattoo machines via clip cord. An artist can adjust the brightness of the panel by adjusting the voltage.",
+    "DE":"Das Panel ist an die Stromzufuhr der Tatoomaschine via clip cord angeschlossen. Der Tätowierer kann die Helligkeit des Panels über die Stromzufuhr regulieren.",
+    "ES":"El panel se puede conectar a fuente de corriente de las máquinas de tattoo via clip cord. El artista puede ajustar el brillo del panel simplemente ajustando el voltaje. ",
+    "Dutch":"Het paneel wordt gevoed met het clip cord dat uit je tattoovoeding komt. Je kunt de helderheid helemaal naar wens instellen door aan de spanning van je voeding te draaien."
+  },
+  ourMission1:{
+    "EN":"Our mission.",
+    "DE":"Unsere Mission",
+    "ES":"Nuestra misión",
+    "Dutch":"Onze Missie"
+  },
+  ourMission2:{
+    "EN":"Improve tattooists environments for the best things birth",
+    "DE":"Das Verbessern der Tätowierumgebung um die besten Lösungen hervorzubringen",
+    "ES":"Mejorar las condiciones de trabajo de los tatuadores para que puedan hacer lo que mejor saben.",
+    "Dutch":"Het verbeteren van de werkplekken van tattoo artists zodat zij de beste resultaten krijgen."
+  },
+  ourValues1:{
+    "EN":"Our values",
+    "DE":"Unsere Werte",
+    "ES":"Nuestros valores",
+    "Dutch":"Onze Waardes"
+  },
+  ourValues2:{
+    "EN":"We do care about the quality of our product to deliver comfort and pleasure into tattooist's workplace",
+    "DE":"Wir legen großen Wert auf die Qualität unserer Produkte um in die Arbeitsumgebung von Tätowierern so angenehm wie möglich zu gestalten",
+    "ES":"Nos preocupamos de que la calidad de nuestros productos para asegurar comodidad y satisfacción en el ambiente de trabajo del tatuador.",
+    "Dutch":"Wij vinden het belangrijk dat onze producten gemakkelijk werken en een tattoo artist een goed geval geven."
+  },
+  ourVision1:{
+    "EN":"Our vision",
+    "DE":"Unsere Vision",
+    "ES":"Nuestra visión",
+    "Dutch":"Onze Visie"
+  },
+  ourVision2:{
+    "EN":"Saw Workshop is the supplier of efficient and pleasurable stuff for tattoo artists well-recognizable in Europe",
+    "DE":"Saw Workshop ist Lieferant effizienter und angenehmer Lösungen für Tätowierer und ist in Europa anerkannt",
+    "ES":"Saw Workshop es un proveedor reconocido en toda Europa de eficacia y satisfacción por nuestros consumidores, los tatuadores.",
+    "Dutch":"Saw Workshop is de leverancier van efficiënte apparatuur voor tattoo artists in heel Europa."
+  },
+  crew:{
+    "EN":"Crew of workshop",
+    "DE":"Werkstatt-Crew",
+    "ES":"Miembros del equipo",
+    "Dutch":"Ons team in de werkplaats"
+  },
+  team1:{
+    "EN":"Team captain,",
+    "DE":"Team Kapitän,",
+    "ES":"Líder del equipo ",
+    "Dutch":"De team captain"
+  },
+  team2:{
+    "EN":"who makes the others work easier.",
+    "DE":"der das Arbeiten der anderen erleichtert.",
+    "ES":"que facilita el trabajo de los demás.",
+    "Dutch":"die het werk makkelijker moet maken voor iedereen bij Saw Workshop"
+  },
+  team3:{
+    "EN":"Strategy, vision, growth.",
+    "DE":"Strategie, Vision, Wachstum",
+    "ES":"Responsable de la estrategia,  visión y desarrollo. ",
+    "Dutch":"Werkt aan: strategie, visie en groei."
+  },
+  technical1:{
+    "EN":"Technical engineer",
+    "DE":"Techniker",
+    "ES":"Ingeniero técnico",
+    "Dutch":"De technische man"
+  },
+  technical2:{
+    "EN":"who designs and assembles panels, gaining maximum usability and efficiency.",
+    "DE":"der Panels mit größtmöglicher Usability und Effizienz entwirft und herstellt.",
+    "ES":"que diseña y ensambla los paneles para obtener.",
+    "Dutch":"die de panelen ontwerpt en in elkaar zet."
+  },
+  technical3:{
+    "EN":"Components supply, producing",
+    "DE":"Komponentenversorgung, Produktion",
+    "ES":"la máxima calidad y eficacia. ",
+    "Dutch":"Verantwoordelijk voor maximaal gebruikersgemak, onderdelen kopen en de productie."
+  },
+  speaker:{
+    "EN":"Speaker who promotes workshop's stuff around the Universe. Customer relationships, promo, PR\n",
+    "DE":"Sprecher, der die Angelegenheiten der Werkstatt in der ganzen Welt vertritt  Kundenbeziehungsmanagement, Promotion, PR",
+    "ES":"Portavoz que promueve el trabajo de nuestro equipo por el resto del mundo. Relación con cliente, promociones y relaciones públicas.",
+    "Dutch":"De PR-persoon van Saw Workshop die onze producten in heel Europa onder de aandacht brent. Verantwoordelijk voor klantenrelaties, promotie en PR."
+  },
+  made1:{
+    "EN":"Made with",
+    "DE":"Made with",
+    "ES":"Hecho con",
+    "Dutch":"Met vakliefde gebouwd"
+  },
+  made2:{
+    "EN":"in Ukraine",
+    "DE":"in Ukraine",
+    "ES":"desde Ucrania",
+    "Dutch":"in de Oekraïne"
+  },
+  show:{
+    "EN":"Show me more",
+    "DE":"Zeig mir mehr",
+    "ES":"Ver más",
+    "Dutch":"Laat me meer zien"
+  },
+  getIn:{
+    "EN":"Get in touch with us",
+    "DE":"Kontaktiere uns",
+    "ES":"Ponte en contacto con nosotros",
+    "Dutch":"Neem contact met ons op"
+  }
+};
